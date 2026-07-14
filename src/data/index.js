ba15c79ff0f888acba4f84bfd05f94b78d2afe8a
STcrafter <Eruanthalone@mail.ru>
2026-07-14 17:38:37 +0400

    Create index.js

---
+++ src/data/index.js
@@ -0,0 +1,15 @@
+import monsters from './monsters.js';
+import spells from './spells.js';
+import classes from './classes.js';
+import races from './races.js';
+import items from './items.js';
+import feats from './feats.js';
+
+export const data = {
+  monsters,
+  spells,
+  classes,
+  races,
+  items,
+  feats,
+};
