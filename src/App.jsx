a4baa177ab3d0c98b413dcfb79661d220e22319d
STcrafter <Eruanthalone@mail.ru>
2026-07-15 15:46:47 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CategoryList from './pages/CategoryList';
 import ItemDetail from './pages/ItemDetail';
+import Constructor from './pages/Constructor'; // ← импорт
 
 function App() {
   return (
-    <BrowserRouter basename="/STHomebrew">  
+    <BrowserRouter basename="/STHomebrew">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category/:category" element={<CategoryList />} />
         <Route path="/category/:category/:id" element={<ItemDetail />} />
+        <Route path="/constructor" element={<Constructor />} /> {/* ← новый маршрут */}
       </Routes>
     </BrowserRouter>
   );
